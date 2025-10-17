7d66bb5a37c0d1bb21566a8b27cb55b00c7e092a
Ducky <ducky@zio.sh>
2025-10-17 17:50:21 +0100

    help: remove hook envvars

--- src/commands/help.sh
+++ src/commands/help.sh
@@ -162,17 +162,12 @@ function atfile.help() {
         Disable periodic update checking when command finishes
     ${_envvar_prefix}_DISABLE_UPDATE_COMMAND <bool¹> (default: $_disable_update_command_default)
         Disable \`update\` command\n
-    ${_envvar_prefix}_HOOK_PRE_START_PATH <path>
-        Path of script to execute on startup
-    ${_envvar_prefix}_HOOK_POST_START_PATH <path>
-        Path of script to execute before command invocation
-    ${_envvar_prefix}_HOOK_PRE_EXIT_PATH <path>
-        Path of script to execute before exit\n
-    ${_envvar_prefix}_DEBUG <bool¹> (default: 0)
+    ${_envvar_prefix}_DEBUG³ <bool¹> (default: 0)
         Print debug outputs
         ⚠️  When output is JSON (${_envvar_prefix}_OUTPUT_JSON=1), sets to 0\n
     ¹ A bool in Bash is 1 (true) or 0 (false)
-    ² These servers are ran by @$handle. You can trust us!"
+    ² These servers are ran by @$handle. You can trust us!
+    ³ Cannot be set by config file"
 
     usage_paths="Paths
     $_path_envvar
