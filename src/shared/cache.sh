226e3498cf62bc4b070681d2e0efd88da8fe2c44
Ducky <ducky@zio.sh>
2025-10-30 17:42:36 +0000

    add debug messages for cache

--- src/shared/cache.sh
+++ src/shared/cache.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
 
+function atfile.cache.debug() {
+    key="$1"
+    action="$2"
+    output="$3"
+
+    [[ -z "$output" ]] && output="(Empty)"
+
+    atfile.say.debug "$action '$key' cache...\n↳ $output"
+}
+
 function atfile.cache.del() {
     key="$(atfile.util.get_cache_path "$1")"
+
+    atfile.cache.debug "$1" "Deleting"
     [[ -f "$key" ]] && rm "$key"
 }
 
 function atfile.cache.get() {
     key="$(atfile.util.get_cache_path "$1")"
-    [[ -f "$key" ]] && cat "$key"
+    unset value
+
+    atfile.cache.debug "$1" "Getting" "$value"
+    [[ -f "$key" ]] && value="$(cat $key)"
+    echo "$value"
 }
 
 function atfile.cache.set() {
     key="$(atfile.util.get_cache_path "$1")"
     value="$2"
 
+    atfile.cache.debug "$1" "Setting" "$value"
     # shellcheck disable=SC2154
     mkdir -p "$_path_cache"
     echo "$value" > "$key"
